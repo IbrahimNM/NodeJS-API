7aa4bdef10ecbd058a2e5374a7b3e57b54d14c26
Ibrahim <almohaimeed.6@wright.edu>
2019-05-09 18:10:34 +0300

    Add package.json for the project

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}`));
+app.get("/info", (req, res) => {
+  var ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  console.log(ip);
+  res.send("This page contains all the information you need.");
+});
+
+app.listen(port, () => console.log(`Server is running on localhost:${port}`));
